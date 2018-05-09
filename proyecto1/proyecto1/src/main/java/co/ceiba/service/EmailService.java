120287bc5ce2b4001b18cad0d069c41fa7d3abfb
Miguel Angel Arenas Correa - Ceiba Software <miguel.arenas@ceiba.com.co>
2018-05-09 16:22:51 -0500

    First commit

---
+++ proyecto1/proyecto1/src/main/java/co/ceiba/service/EmailService.java
@@ -0,0 +1,7 @@
+package co.ceiba.service;
+
+public class EmailService {
+	public String sendEmail(String message) {
+		return "Email enviado correctamento con el mensaje " + message;
+	}
+}
---
+++ proyecto1/proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -0,0 +1,27 @@
+package co.ceiba.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+import org.junit.Assert;
+
+public class NotifyPersonServiceTest {
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+	
+	@Before
+	public void setup() {
+		emailService = new EmailService();
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+	
+	@Test
+	public void notifyTest() {
+		Person person = new PersonTestDataBuilder().build();
+		String message = notifyPersonService.notify(person);
+		Assert.assertNotNull(message);
+	}
+}
